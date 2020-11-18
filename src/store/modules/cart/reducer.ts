8522911af7aecc76ca57b5b902301b6e13585993
DanielSLucas <daniellucas-pms@hotmail.com>
2020-11-18 18:05:01 -0300

    redux types and cart component

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,4 +1,5 @@
 import { Reducer } from "redux";
+import produce from 'immer';
 import { ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
@@ -6,25 +7,26 @@ const INITIAL_STATE: ICartState = {
 };
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
-  switch(action.type) {
-    case 'ADD_PRODUCT_TO_CART': {
-      const { product } = action.payload;
-
-      return {
-        ...state,
-        items: [
-          ...state.items,
+  return produce(state, draft => {
+    switch (action.type) {
+      case 'ADD_PRODUCT_TO_CART': {
+        const { product } = action.payload;
+        
+        draft.items.push(
           {
             product,
             quantity: 1,
           }
-        ]
-      };
+        );
+
+        break
+      }
+      default: {
+        return draft;
+      }
     }
-    default: {
-      return state;
-    }
-  }
+  });    
+    
 }
 
 export default cart;
