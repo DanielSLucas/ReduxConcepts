7a28e3792eb23a810d739287c8df7054f2721a31
DanielSLucas <daniellucas-pms@hotmail.com>
2020-11-18 16:33:09 -0300

    creating actions

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,13 +1,45 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import api from '../services/api';
+import { addProductToCart } from '../store/modules/cart/actions';
+import { IProduct } from '../store/modules/cart/types';
+
 
 const Catalog: React.FC = () => {
-  const state = useSelector(state => state);
+  const dispatch = useDispatch();
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
 
-  console.log(state);
-  
+  useEffect(() => {
+    api.get('products').then(response => {
+      setCatalog(response.data);
+    })
+  }, []);
+
+  const handleAddProductToCart = useCallback((product: IProduct) => {
+    dispatch(addProductToCart(product))
+  }, [])
+
   return (
-    <h1>Catalog</h1>
+    <main>
+      <h1>Catalog</h1>
+
+      {catalog.map(product => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> {" - "}
+          <span>{product.price}</span> {"  "}
+
+          <button 
+            type="button" 
+            onClick={() => handleAddProductToCart(product)}
+          >
+            Comprar
+          </button>
+        </article>
+      ))}
+    </main>
+
+
   );
 }
 
