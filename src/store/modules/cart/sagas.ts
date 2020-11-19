5ba5682798cdb0450a1eeda2426280a2408aa9d1
DanielSLucas <daniellucas-pms@hotmail.com>
2020-11-19 11:53:31 -0300

    getting cart's product quantity

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,7 +1,17 @@
-import { all, takeLatest } from 'redux-saga/effects';
+import { all, select, takeLatest } from 'redux-saga/effects';
+import { IState } from '../..';
+import { addProductToCart } from './actions';
 
-function checkProductStock() {
-  console.log('Adicionou ao carrinho');
+type CheckProductStockRequest = ReturnType<typeof addProductToCart>
+
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product }  = payload;
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0;
+  });
+
+  console.log(currentQuantity);
 }
 
 export default all([
